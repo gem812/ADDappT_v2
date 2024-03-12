c4164801b2442f3d234a5d3f245d206bcb24048e
40220321 <gohare10@qub.ac.uk>
2024-03-12 20:43:09 +0000

    set up the requisite app module in preparation for room db set up

--- app/src/main/java/com/example/addappt/AddapptApplication.kt
+++ app/src/main/java/com/example/addappt/AddapptApplication.kt
@@ -1,4 +1,8 @@
 package com.example.addappt
 
-class AddapptApplication {
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AddapptApplication : Application() {
 }
