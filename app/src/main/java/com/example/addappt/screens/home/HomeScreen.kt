95567620e42b122d072ac7b8d34df82b6a8697f0
40220321 <gohare10@qub.ac.uk>
2024-03-12 13:54:00 +0000

    Implemented BarDrawer interface for chart library, added scaffold to stats screen

--- app/src/main/java/com/example/addappt/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/addappt/screens/home/HomeScreen.kt
@@ -1,7 +1,5 @@
 package com.example.addappt.screens.home
 
-import android.content.Intent
-import android.provider.Settings
 import android.util.Log
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
@@ -37,7 +35,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.content.ContextCompat.startActivity
 import com.example.addappt.R
 import com.example.addappt.data.ScreenTimeCalculator
 import com.example.addappt.models.ui.CarouselContentsModel
--- app/src/main/java/com/example/addappt/screens/stats/StatsScreen.kt
+++ app/src/main/java/com/example/addappt/screens/stats/StatsScreen.kt
@@ -1,9 +1,36 @@
 package com.example.addappt.screens.stats
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.sp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun StatsScreen(){
-    Text(text = "Stats Screen")
+    val context = LocalContext.current
+
+    Scaffold (
+        topBar = {
+            TopAppBar(
+                title = { Text("Home", fontSize = 24.sp) }
+            )
+        },
+        content = {
+            Column(
+                modifier = Modifier
+                    .padding(it)
+                    .fillMaxSize(),
+            ){
+
+            }
+        }
+    )
 }
