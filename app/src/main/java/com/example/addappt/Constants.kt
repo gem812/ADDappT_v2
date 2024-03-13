16a44307257d4f10fdbeaf20677a07637c82bb6b
40220321 <gohare10@qub.ac.uk>
2024-03-13 12:29:53 +0000

    hiding API key from git in local.properties gradle file

---
+++ app/src/main/java/com/example/addappt/Constants.kt
@@ -0,0 +1,14 @@
+package com.example.addappt
+
+import com.example.addappt.BuildConfig
+
+object Constants {
+
+    object QuoteApi{
+
+        const val BASE_URL = ""
+        const val API_KEY = BuildConfig.API_KEY
+
+    }
+
+}
